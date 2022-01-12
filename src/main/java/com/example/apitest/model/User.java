a7df18f7682659c9b20f811bbb5edce983dfba1b
fHuteau <57004056+fHuteau@users.noreply.github.com>
2022-01-12 15:53:30 +0100

    modif

--- src/main/java/com/example/apitest/model/User.java
+++ src/main/java/com/example/apitest/model/User.java
@@ -3,7 +3,7 @@ package com.example.apitest.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
